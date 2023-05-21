594585e650619b9bbf8a34db8f8b35b5a5593db7
Naama <naamap870@gmail.com>
2023-05-21 14:51:36 +0300

    21/05 firebase

--- src/firebase.js
+++ src/firebase.js
@@ -18,13 +18,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyD8P3Y6DxvI8TARTfrHLViBztFYpHvOMSE",
+  authDomain: "my-crypto-website.firebaseapp.com",
+  projectId: "my-crypto-website",
+  storageBucket: "my-crypto-website.appspot.com",
+  messagingSenderId: "827217601983",
+  appId: "1:827217601983:web:9941dd377c7199fa752d0a",
+  measurementId: "G-E4GPYPW0ZZ"
 };
 
 const app = initializeApp(firebaseConfig);
